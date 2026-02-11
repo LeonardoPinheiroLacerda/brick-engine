ad9298075e170eb9e7d3daf02be2901290d296aa
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2026-02-11 12:24:33 -0300

    feat: static files

--- src/resources.ts
+++ src/resources.ts
@@ -1,20 +1,27 @@
 // Dynamically import all CSS files from public/style
-const styleContext = (require as any).context('../public/style', false, /\.css$/);
+const styleContext = (require as any).context(
+    '../public/style',
+    false,
+    /\.css$/,
+);
 styleContext.keys().forEach(styleContext);
 
-// // Dynamically import all WAV files from public/sounds
-// const soundContext = require.context('../public/sounds', false, /\.wav$/);
-// soundContext.keys().forEach(soundContext);
+// Dynamically import all WAV files from public/sounds
+const soundContext = (require as any).context(
+    '../public/sounds',
+    false,
+    /\.wav$/,
+);
+soundContext.keys().forEach(soundContext);
 
-// // Dynamically import all image files from public/images
-// const imageContext = require.context(
-//     '../public/images',
-//     false,
-//     /\.(png|gif|svg)$/,
-// );
-// imageContext.keys().forEach(imageContext);
+// Dynamically import all image files from public/images
+const imageContext = (require as any).context(
+    '../public/images',
+    false,
+    /\.(png|gif|svg)$/,
+);
+imageContext.keys().forEach(imageContext);
 
-// // Explicit imports for specific files as they are handled individually by webpack config
-// import '../public/favicon.ico';
-// import '../public/CNAME';
-// import '../public/modal'; // Assuming this refers to 'modal.js' and is treated as an asset.
+// Explicit imports for specific files as they are handled individually by webpack config
+import '../public/favicon.ico';
+import '../public/CNAME';
