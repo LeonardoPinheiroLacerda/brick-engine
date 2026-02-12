8f1a86fe306a4d0bdab11621cc4dce4e53ae4927
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2026-02-12 17:43:23 -0300

    refactor: initialize all modules via a loop

--- src/core/Game.ts
+++ src/core/Game.ts
@@ -1,14 +1,12 @@
 import P5 from 'p5';
+import GameView from '../view/GameView';
+import GameControl from './module/control/GameControl';
 import GameGrid from './module/grid/GameGrid';
 import GameRenderer from './module/renderer/GameRenderer';
-import GameText from './module/text/GameText';
 import GameState from './module/state/GameState';
-import GameControl from './module/control/GameControl';
-import { FontSize, FontAlign, FontVerticalAlign, ControlKey } from './types/enums';
+import GameText from './module/text/GameText';
 import { Initializable } from './types/Interfaces';
-import { RendererMetrics, GameModules } from './types/Types';
-import { Grid, RendererComposite, Text, State, Control } from './types/modules';
-import GameView from '../view/GameView';
+import { GameModules } from './types/Types';
 
 export default class Game implements Initializable {
     private _p: P5;
@@ -36,13 +34,11 @@ export default class Game implements Initializable {
             control: new GameControl(),
         };
 
-        this._modules.renderer.setup();
-        this._modules.grid.setup();
-        this._modules.text.setup();
-        this._modules.state.setup();
-        this._modules.control.setup();
+        Object.values(this._modules).forEach(module => {
+            (module as Initializable).setup();
+        });
 
-        const { grid, text, state, control, renderer } = this._modules;
+        const { text, control, renderer } = this._modules;
 
         control.setModules(this._modules);
 
@@ -52,18 +48,7 @@ export default class Game implements Initializable {
     }
 
     draw() {
-        const { text, renderer, grid } = this._modules;
-
+        const { renderer, grid } = this._modules;
         renderer.render(grid.getGrid());
-
-        text.setActiveText();
-        text.setTextAlign(FontAlign.CENTER, FontVerticalAlign.CENTER);
-        text.setTextSize(FontSize.MEDIUM);
-        text.textOnDisplay('Hello World', { x: 0.5, y: 0.5 });
-
-        text.setInactiveText();
-        text.setTextAlign(FontAlign.LEFT, FontVerticalAlign.TOP);
-        text.setTextSize(FontSize.EXTRA_SMALL);
-        text.textOnHud('Hello World', { x: 0, y: 0 });
     }
 }
